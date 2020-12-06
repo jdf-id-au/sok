5a7738a8274f4d53012bedc9f508e00f59c3b6b8
Jeremy Field <jdf@jdf.id.au>
2020-12-06 21:08:02 +1100

    need to finish wiring

--- src/sok/api.clj
+++ src/sok/api.clj
@@ -4,7 +4,7 @@
   (:import (io.netty.bootstrap ServerBootstrap)
            (io.netty.channel ChannelHandlerContext
                              ChannelInitializer)
-           (io.netty.channel SimpleChannelInboundHandler)
+           (io.netty.channel SimpleChannelInboundHandler ChannelFutureListener)
            (io.netty.channel.nio NioEventLoopGroup)
            (io.netty.channel.group ChannelGroup)
            (io.netty.channel.socket SocketChannel)
@@ -18,12 +18,19 @@
            (io.netty.channel.group DefaultChannelGroup)
            (io.netty.util.concurrent GlobalEventExecutor)))
 
-(defn ws-handler [clients]
+(defn ws-handler [channel-group clients]
   (proxy [SimpleChannelInboundHandler] []
-    (channelActive [^ChannelHandlerContext ctx] ; NB confused about @Skip annotation in superclass
-      (let [ch (.channel ctx)]
-        (.add clients ch)
-        (proxy-super channelActive ctx)))
+    (channelActive [^ChannelHandlerContext ctx]
+      (let [ch (.channel ctx)
+            cf (.closeFuture ch)]
+        (try (.add channel-group ch)
+             (swap! clients assoc (.id ch) nil) ; TODO metadata would be stored here
+             (.addListener cf (proxy [ChannelFutureListener] []
+                                (operationComplete [_]
+                                  (swap! clients dissoc (.id ch)))))
+             (catch Exception e
+               (log/error e "Unable to track channel" ch)))
+        (.fireChannelActive ctx))) ; ? from ChannelInboundHandlerAdapter, although annotated @Skip ...
     (channelRead0 [^ChannelHandlerContext ctx
                    ^TextWebSocketFrame frame]
       (let [ch (.channel ctx)
@@ -35,17 +42,16 @@
                               :or {max-frame-size (* 64 1024) ; TODO actually plug in
                                    max-message-size (* 1024 1024)}
                               :as opts}
-                ^ChannelGroup clients]
+                ^ChannelGroup channel-group
+                clients]
   (proxy [ChannelInitializer] []
     (initChannel [^SocketChannel ch]
-      ; TODO add channel to client registry, allow gathering metadata (e.g. logged in user)
-      ; TODO add listener to (.closeFuture ch) to  remove channel from client registry
       (doto (.pipeline ch)
         (.addLast "http" (HttpServerCodec.))
         (.addLast "http-agg" (HttpObjectAggregator. (* 64 1024))) ; probably not necessary?
         (.addLast "ws" (WebSocketServerProtocolHandler. path))
         (.addLast "ws-agg" (WebSocketFrameAggregator. max-message-size))
-        (.addLast "ws-handler" (ws-handler clients))))))
+        (.addLast "ws-handler" (ws-handler channel-group clients))))))
         ; TODO per message deflate?)
 
 (defprotocol SokServer "Connect Netty websocket server with core.async channels."
@@ -57,12 +63,15 @@
   ([port] (server! port "/" nil))
   ([port path opts]
    (let [loop-group (NioEventLoopGroup.) ; TODO look at aleph for epoll, thread number specification
-         clients (DefaultChannelGroup. GlobalEventExecutor/INSTANCE)] ; single threaded executor for group actions
+         channel-group (DefaultChannelGroup. GlobalEventExecutor/INSTANCE) ; single threaded executor for group actions
+         ; channel-group not flexible enough for client metadata but does track the channels
+         ; therefore store metadata in parallel atom...
+         clients (atom {})]
      (try (let [bootstrap (doto (ServerBootstrap.)
                             (.group loop-group) ; TODO any need for separate parent and child groups?
                             (.channel NioServerSocketChannel)
                             (.localAddress ^int (InetSocketAddress. port))
-                            (.childHandler (pipeline path opts clients)))
+                            (.childHandler (pipeline path opts channel-group clients)))
                 server-channel (-> bootstrap .bind .sync)]
             (reify
               Closeable
