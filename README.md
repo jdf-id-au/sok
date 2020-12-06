95a6b8ddc8d3474da375a53b6e702753c936fdc8
Jeremy Field <jdf@jdf.id.au>
2020-12-06 14:13:17 +1100

    opts, licence, readme

---
+++ README.md
@@ -0,0 +1,5 @@
+# jdf/sok
+
+Text over Netty websockets with as little as possible else.
+
+Some ideas from [Aleph](https://github.com/aleph-io/aleph), thank you.
--- src/sok/api.clj
+++ src/sok/api.clj
@@ -22,37 +22,41 @@
       (do (println "server received" ctx (count (.text frame)))
         (-> ctx .channel (.writeAndFlush (.retain frame)))))))
 
-(defn pipeline [^String path]
+(defn pipeline [^String path {:keys [max-frame-size max-message-size]
+                              :or {max-frame-size (* 64 1024) ; TODO actually plug in
+                                   max-message-size (* 1024 1024)}
+                              :as opts}]
   (proxy [ChannelInitializer] []
     (initChannel [^SocketChannel ch]
       (doto (.pipeline ch)
         (.addLast "http" (HttpServerCodec.))
         (.addLast "http-agg" (HttpObjectAggregator. (* 64 1024)))
         (.addLast "ws" (WebSocketServerProtocolHandler. path))
-        (.addLast "ws-agg" (WebSocketFrameAggregator. (* 1024 1024)))
+        (.addLast "ws-agg" (WebSocketFrameAggregator. max-message-size))
         (.addLast "ws-handler" (ws-handler))))))
         ; TODO per message deflate?)
 
 (defn server!
-  [port path]
-  (let [group (NioEventLoopGroup.)] ; TODO look at aleph for epoll, thread number specification
-    (try (let [bs (doto (ServerBootstrap.)
-                    (.group group) ; TODO any need for separate parent and child groups?
-                    (.channel NioServerSocketChannel)
-                    (.localAddress ^int (InetSocketAddress. port))
-                    (.childHandler (pipeline path)))
-               ch (-> bs .bind .sync .channel)]
-           (reify
-             Closeable
-             (close [_]
-               (do (some-> ch .close)
-                   (-> group .shutdownGracefully)))
-             Object
-             (toString [_]
-               (format "Sok[port:%d, path:%s]" port path))))
-         (catch Exception e
-           (-> group .shutdownGracefully .sync)
-           (throw e)))))
+  ([port] (server! port "/" nil))
+  ([port path opts]
+   (let [group (NioEventLoopGroup.)] ; TODO look at aleph for epoll, thread number specification
+     (try (let [bs (doto (ServerBootstrap.)
+                     (.group group) ; TODO any need for separate parent and child groups?
+                     (.channel NioServerSocketChannel)
+                     (.localAddress ^int (InetSocketAddress. port))
+                     (.childHandler (pipeline path opts)))
+                ch (-> bs .bind .sync .channel)]
+            (reify
+              Closeable
+              (close [_]
+                (do (some-> ch .close)
+                    (-> group .shutdownGracefully)))
+              Object
+              (toString [_]
+                (format "Sok[port:%d, path:%s]" port path))))
+          (catch Exception e
+            (-> group .shutdownGracefully .sync)
+            (throw e))))))
 
-#_ (def server (server! 8123 "/"))
+#_ (def server (server! 8123))
 #_ (.close server)
