772d9f7958ebd6d634610bc28d1d0b5d2d4734a0
Jeremy Field <jeremy.field@gmail.com>
2020-12-06 22:54:23 +1100

    core.async chans connected to netty websocket channels!

--- dev/user.clj
+++ dev/user.clj
@@ -1,9 +1,20 @@
 (ns user
-  (:require [hato.websocket :as ws]))
+  (:require [sok.api :as sok]
+            [clojure.core.async :as async :refer [chan go >! <! >!! <!!]]
+            [hato.websocket :as ws]))
 
-#_ (let [w @(ws/websocket "ws://localhost:8123/"
-              {:on-message (fn [ws msg last?] (println "client received" (if last? "(last)") msg))
-               :on-close (fn [ws status reason] (println "client ws closed" status reason))})]
-     (ws/send! w #_"hi there" (apply str (interpose \space (range 10000))))
-     (Thread/sleep 500)
-     (ws/close! w))
+(def server (sok/server! 8123))
+
+#_ ((:close server))
+
+#_ (def w @(ws/websocket "ws://localhost:8123"
+             ; TODO need to concat strings
+             {:on-message (fn [ws msg last?] (println "client received" (if last? "(last)") msg))
+              :on-close (fn [ws status reason] (println "client ws closed" status reason))}))
+
+#_ (ws/send! w #_"hi there" (apply str (interpose \space (range 10000))))
+#_ (ws/close! w)
+
+#_ (<!! (:in server))
+#_ (def id (first *1))
+#_ (async/put! (:out server) [id "oopsie"])
--- src/sok/api.clj
+++ src/sok/api.clj
@@ -1,6 +1,7 @@
 (ns sok.api
   (:require [clojure.tools.logging :as log]
-            [clojure.string :as str])
+            [clojure.string :as str]
+            [clojure.core.async :as async :refer [go chan >! <! >!! <!!]])
   (:import (io.netty.bootstrap ServerBootstrap)
            (io.netty.channel ChannelHandlerContext
                              ChannelInitializer)
@@ -14,17 +15,16 @@
                                                    TextWebSocketFrame WebSocketFrameAggregator)
            (io.netty.handler.codec.http HttpServerCodec
                                         HttpObjectAggregator)
-           (java.io Closeable)
            (io.netty.channel.group DefaultChannelGroup)
            (io.netty.util.concurrent GlobalEventExecutor)))
 
-(defn ws-handler [channel-group clients]
+(defn ws-handler [channel-group clients in]
   (proxy [SimpleChannelInboundHandler] []
     (channelActive [^ChannelHandlerContext ctx]
       (let [ch (.channel ctx)
             cf (.closeFuture ch)]
         (try (.add channel-group ch)
-             (swap! clients assoc (.id ch) nil) ; TODO metadata would be stored here
+             (swap! clients assoc (.id ch) {:addr (.remoteAddress ch)}) ; add more metadata in application
              (.addListener cf (proxy [ChannelFutureListener] []
                                 (operationComplete [_]
                                   (swap! clients dissoc (.id ch)))))
@@ -34,16 +34,18 @@
     (channelRead0 [^ChannelHandlerContext ctx
                    ^TextWebSocketFrame frame]
       (let [ch (.channel ctx)
+            id (.id ch)
             msg (str "server received" (count (.text frame)) "bytes from"
                   (.remoteAddress ch) "on channel id" (.id ch))]
-        (.writeAndFlush ch (TextWebSocketFrame. msg))))))
+        (async/put! in [id (.text frame)])
+        #_(.writeAndFlush ch (TextWebSocketFrame. msg))))))
 
 (defn pipeline [^String path {:keys [max-frame-size max-message-size]
                               :or {max-frame-size (* 64 1024) ; TODO actually plug in
                                    max-message-size (* 1024 1024)}
                               :as opts}
                 ^ChannelGroup channel-group
-                clients]
+                clients in]
   (proxy [ChannelInitializer] []
     (initChannel [^SocketChannel ch]
       (doto (.pipeline ch)
@@ -51,7 +53,7 @@
         (.addLast "http-agg" (HttpObjectAggregator. (* 64 1024))) ; probably not necessary?
         (.addLast "ws" (WebSocketServerProtocolHandler. path))
         (.addLast "ws-agg" (WebSocketFrameAggregator. max-message-size))
-        (.addLast "ws-handler" (ws-handler channel-group clients))))))
+        (.addLast "ws-handler" (ws-handler channel-group clients in))))))
         ; TODO per message deflate?)
 
 (defprotocol SokServer "Connect Netty websocket server with core.async channels."
@@ -66,29 +68,25 @@
          channel-group (DefaultChannelGroup. GlobalEventExecutor/INSTANCE) ; single threaded executor for group actions
          ; channel-group not flexible enough for client metadata but does track the channels
          ; therefore store metadata in parallel atom...
-         clients (atom {})]
+         clients (atom {})
+         in (chan)
+         out (chan)
+         _ (go (while true ; TODO kill elegantly
+                 (let [[id ^String msg] (<! out)
+                       ch (.find channel-group id)]
+                   (log/info "About to try writing" msg "to" ch \( id \))
+                   (if ch (.writeAndFlush ch (TextWebSocketFrame. msg))
+                          (log/warn "Tried to write to closed channel" id (get @clients id) msg)))))]
      (try (let [bootstrap (doto (ServerBootstrap.)
                             (.group loop-group) ; TODO any need for separate parent and child groups?
                             (.channel NioServerSocketChannel)
                             (.localAddress ^int (InetSocketAddress. port))
-                            (.childHandler (pipeline path opts channel-group clients)))
+                            (.childHandler (pipeline path opts channel-group clients in)))
                 server-channel (-> bootstrap .bind .sync)]
-            (reify
-              Closeable
-              (close [_]
-                (do (some-> server-channel .close)
-                    ; TODO should shut down client channels? see `ChannelGroup`
-                    (-> loop-group .shutdownGracefully)))
-              Object
-              (toString [_]
-                (format "Sok[port:%d, path:%s]" port path))
-              SokServer
-              (in [_])
-              (out [_])
-              (clients [_])))
+            {:close (fn [] (some-> server-channel .channel .close)
+                           ; TODO should shut down client channels? see `ChannelGroup`
+                           (-> loop-group .shutdownGracefully))
+             :port port :path path :in in :out out :clients clients})
           (catch Exception e
             (-> loop-group .shutdownGracefully .sync)
             (throw e))))))
-
-#_ (def server (server! 8123))
-#_ (.close server)
