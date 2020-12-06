340ba8dd1c30700337dc3ffb54ead56bcf33d98c
Jeremy Field <jdf@jdf.id.au>
2020-12-06 13:48:51 +1100

    simple echo server

---
+++ dev/user.clj
@@ -0,0 +1,9 @@
+(ns user
+  (:require [hato.websocket :as ws]))
+
+#_ (let [w @(ws/websocket "ws://localhost:8123/"
+              {:on-message (fn [ws msg last?] (println "client received" (if last? "(last)") (count msg)))
+               :on-close (fn [ws status reason] (println "client ws closed" status reason))})]
+     (ws/send! w #_"hi there" (apply str (interpose \space (range 10000))))
+     (Thread/sleep 500)
+     (ws/close! w))
---
+++ src/sok/api.clj
@@ -0,0 +1,58 @@
+(ns sok.api
+  (:require [clojure.tools.logging :as log]
+            [clojure.string :as str])
+  (:import (io.netty.bootstrap ServerBootstrap)
+           (io.netty.channel ChannelHandlerContext
+                             ChannelInitializer)
+           (io.netty.channel SimpleChannelInboundHandler)
+           (io.netty.channel.nio NioEventLoopGroup)
+           (io.netty.channel.socket SocketChannel)
+           (io.netty.channel.socket.nio NioServerSocketChannel)
+           (java.net InetSocketAddress)
+           (io.netty.handler.codec.http.websocketx WebSocketServerProtocolHandler
+                                                   TextWebSocketFrame WebSocketFrameAggregator)
+           (io.netty.handler.codec.http HttpServerCodec
+                                        HttpObjectAggregator)
+           (java.io Closeable)))
+
+(defn ws-handler []
+  (proxy [SimpleChannelInboundHandler] []
+    (channelRead0 [^ChannelHandlerContext ctx
+                   ^TextWebSocketFrame frame]
+      (do (println "server received" ctx (count (.text frame)))
+        (-> ctx .channel (.writeAndFlush (.retain frame)))))))
+
+(defn pipeline [^String path]
+  (proxy [ChannelInitializer] []
+    (initChannel [^SocketChannel ch]
+      (doto (.pipeline ch)
+        (.addLast "http" (HttpServerCodec.))
+        (.addLast "http-agg" (HttpObjectAggregator. (* 64 1024)))
+        (.addLast "ws" (WebSocketServerProtocolHandler. path))
+        (.addLast "ws-agg" (WebSocketFrameAggregator. (* 1024 1024)))
+        (.addLast "ws-handler" (ws-handler))))))
+        ; TODO per message deflate?)
+
+(defn server!
+  [port path]
+  (let [group (NioEventLoopGroup.)] ; TODO look at aleph for epoll, thread number specification
+    (try (let [bs (doto (ServerBootstrap.)
+                    (.group group) ; TODO any need for separate parent and child groups?
+                    (.channel NioServerSocketChannel)
+                    (.localAddress ^int (InetSocketAddress. port))
+                    (.childHandler (pipeline path)))
+               ch (-> bs .bind .sync .channel)]
+           (reify
+             Closeable
+             (close [_]
+               (do (some-> ch .close)
+                   (-> group .shutdownGracefully)))
+             Object
+             (toString [_]
+               (format "Sok[port:%d, path:%s]" port path))))
+         (catch Exception e
+           (-> group .shutdownGracefully .sync)
+           (throw e)))))
+
+#_ (def server (server! 8123 "/"))
+#_ (.close server)
